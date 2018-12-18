c3a7c7c212afb8a61e933dbf6444b225b419c4b9
locle <letanloc1941995@gmail.com>
2018-12-18 09:59:23 +0700

    create user list screen

---
+++ app/src/main/java/publishing/h/bm/unitest/demo3/view/DbActivity.kt
@@ -0,0 +1,16 @@
+package publishing.h.bm.unitest.demo3.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import publishing.h.bm.unitest.R
+
+class DbActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_db)
+
+    }
+
+
+}
---
+++ app/src/main/java/publishing/h/bm/unitest/demo3/view/UserViewModel.kt
@@ -0,0 +1,9 @@
+package publishing.h.bm.unitest.demo3.view
+
+
+class UserViewModel {
+
+    fun initDB() {
+
+    }
+}
